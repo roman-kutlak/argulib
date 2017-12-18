f1530d240f79ddd9bc9387a7a7f420084dcafb8b
Roman Kutlak <roman@kutlak.net>
2017-12-18 16:08:55 +0000

    Added setup.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='argulib',
+    version='0.1',
+    packages=['test', 'argulib'],
+    url='https://github.com/roman-kutlak/argulib',
+    license='BSD',
+    author='Roman Kutlak',
+    author_email='kutlak.roman@gmail.com',
+    description='A simple library for formal argumentation'
+)
